3aa3665a887975988a1e1a74933c1c0e711e593d
AndreaZentriegen <andrea.zentriegen@gmail.com>
2024-11-20 18:05:15 +0100

    Hamburger Menu hinzugefügt und Skalierbarkeit verbesser

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-.then(function(response) {
-    alert("Nachricht erfolgreich gesendet!");
-    document.getElementById("contact-form").reset(); // Formular zurücksetzen
-  })
-  
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const navbar = document.querySelector(".navbar");
+  const navLinks = document.querySelectorAll(".navbar ul li a");
+
+  // Öffnen/Schließen des Menüs
+  hamburger.addEventListener("click", () => {
+    navbar.classList.toggle("nav-open");
+  });
+
+  // Schließen des Menüs nach einem Klick auf einen Link
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      navbar.classList.remove("nav-open");
+    });
+  });
+});
